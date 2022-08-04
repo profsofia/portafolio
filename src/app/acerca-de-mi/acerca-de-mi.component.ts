d39eb1ee4c8cc2c374ecc70e041c2dc856d2d245
sofia <sofiainesschenone@gmail.com>
2022-08-04 15:10:40 -0300

    se terminó la sección acerca de mi

--- src/app/acerca-de-mi/acerca-de-mi.component.ts
+++ src/app/acerca-de-mi/acerca-de-mi.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercaDeMiComponent implements OnInit {
   list = ['Sofía Schenone']
+  links= [
+    {icon:"uil uil-linkedin", href:"https://www.linkedin.com/in/sofiaschenone/"},
+    {icon:"uil uil-youtube", href:"https://www.youtube.com/channel/UCcSErMMrU9eYeT93L-87n-w"},
+    //{icon:"uil uil-github-alt", href:"https://github.com/profsofia"},
+    {icon:"uil uil-envelope", href:"sofiainesschenone@gmail.com"}
+  ]
   constructor() { }
 
   ngOnInit(): void {
